c6c970d04acd72e901a9e01cce50f83763a9a69d
MrM3hran <nikrouz1011@gmail.com>
2025-04-04 17:38:05 +0330

    آپدیت چا با

--- app.vue
+++ app.vue
@@ -1,5 +1,17 @@
+<script setup>
+import { onMounted } from "vue";
+
+onMounted(() => {
+  const script = document.createElement("script");
+  script.src = "https://cdn.pulse.is/livechat/loader.js";
+  script.setAttribute("data-live-chat-id", "67efd120a3b3b16ebf0110d0");
+  script.async = true;
+  document.body.appendChild(script);
+});
+</script>
+
 <template>
   <div>
-    <NuxtPage/>
+    <NuxtPage />
   </div>
 </template>

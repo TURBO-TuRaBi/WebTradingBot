b1e46fbea2dab244dd0acd06fb8e094bb42857ee
MrM3hran <nikrouz1011@gmail.com>
2025-04-04 18:20:05 +0330

    s

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,16 @@ export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss'],
   compatibilityDate: '2025-04-02',
   ssr: false,
-  
-});
+
+  app: {
+    head: {
+      script: [
+        {
+          src: "https://embed.tawk.to/67efeba42980661919b8fe4d/1io0j0vj0",
+          async: true,
+          crossorigin: "anonymous"
+        }
+      ]
+    }
+  }
+})

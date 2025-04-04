d63a855c24d492ee0ecbc45d5377c1d9c9acaf2e
MrM3hran <nikrouz1011@gmail.com>
2025-04-04 17:53:33 +0330

    s

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,15 +2,5 @@ export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss'],
   compatibilityDate: '2025-04-02',
   ssr: false,
-  app: {
-    head: {
-      script: [
-        {
-          src: 'https://cdn.pulse.is/livechat/loader.js',
-          'data-live-chat-id': '67efd120a3b3b16ebf0110d0',
-          async: true
-        }
-      ]
-    }
-  }
+  
 });
